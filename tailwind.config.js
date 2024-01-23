9162e87f196863db26913aaacdcafed288d82755
Maniteja0126 <maniteja2601@gmail.com>
2024-01-23 18:23:09 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [],
+  theme: {
+    
+    extend: {fontFamily: {
+      "inter":["Inter" , "sans-serif"],
+    }},
+  },
+  plugins: [],
+});
